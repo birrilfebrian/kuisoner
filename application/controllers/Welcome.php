75ddfbc0509938aa5c5928375ffc18f20fac2b0e
birrilfebrian <55568576+birrilfebrian@users.noreply.github.com>
2021-04-15 19:36:13 +0700

    pertama + login

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,55 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('login');
+	}
+
+	 public function dashboard()
+	{
+		if(empty($this->session->userdata('username'))){
+			redirect('welcome');
+		}else{
+			$this->load->view('partial/header');
+			$this->load->view('partial/sidebar');
+			$this->load->view('partial/navbar');
+			$this->load->view('dashboard');
+			$this->load->view('partial/footer');
+		}
+	}
+
+	public function ceklogin(){
+		$user = $this->input->post('username');
+		$password = $this->input->post('pass');
+		if($user == "admin"){
+			if($password == "admin"){
+				$this->session->set_userdata('username',$user);
+				$this->dashboard();
+			}else{
+				$this->session->set_flashdata('pesan','<div class="text-center w-full p-t-25 p-b-10" >Password Salah!</div>');
+				redirect('welcome');
+			}
+		}else{
+			$this->session->set_flashdata('pesan','<div class="text-center w-full p-t-25 p-b-10" >Username Salah!</div>');
+			redirect('welcome');
+		}
+	}
+}
---
+++ application/views/login.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Login V12</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="<?= base_url()?>assets/images/icons/favicon.ico" />
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/vendor/bootstrap/css/bootstrap.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/vendor/animate/animate.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/vendor/css-hamburgers/hamburgers.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/vendor/select2/select2.min.css">
+	<link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet" />
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/util.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/css/main.css">
+	<!--===============================================================================================-->
+</head>
+
+<body>
+
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/img-01.jpg');">
+			<div class="wrap-login100 p-t-190 p-b-30">
+				<form action="<?= base_url()?>welcome/ceklogin" method="post" class="login100-form validate-form">
+					<span class="login100-form-title p-t-20 p-b-45">
+						<?php 
+						if(!empty($this->session->flashdata('pesan'))){
+							echo $this->session->flashdata('pesan');
+						}else{ ?>
+						Silahkan Login
+						<?php } ?>
+					</span>
+
+					<div class="wrap-input100 validate-input m-b-10" data-validate="Username is required">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-user"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-10" data-validate="Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock"></i>
+						</span>
+					</div>
+
+					<div class="container-login100-form-btn p-t-10">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center w-full p-t-25 p-b-230">
+						<a href="#" class="txt1">
+							Forgot Username / Password?
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	<!--===============================================================================================-->
+	<script src="<?= base_url()?>assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="<?= base_url()?>assets/vendor/bootstrap/js/popper.js"></script>
+	<script src="<?= base_url()?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="<?= base_url()?>assets/vendor/select2/select2.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="<?= base_url()?>assets/js/main.js"></script>
+
+</body>
+
+</html>
