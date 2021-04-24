bd759ee0d2bd9c51fdb5225859c6b1a1fafd079a
birrilfebrian <55568576+birrilfebrian@users.noreply.github.com>
2021-04-24 14:54:30 +0700

    Create quisoner.sql

---
+++ quisoner.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2021 at 09:54 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quisoner`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_hasil`
+--
+
+CREATE TABLE `tb_hasil` (
+  `id_hasil` varchar(255) NOT NULL,
+  `id_paket` varchar(255) NOT NULL,
+  `id_user` varchar(255) NOT NULL,
+  `total_point` int(5) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_paket`
+--
+
+CREATE TABLE `tb_paket` (
+  `id_paket` varchar(255) NOT NULL,
+  `umur` int(2) NOT NULL,
+  `jenis` int(11) NOT NULL COMMENT '0=kpsp,1=tdd,2=tdl\r\n'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_soal`
+--
+
+CREATE TABLE `tb_soal` (
+  `id_soal` varchar(255) NOT NULL,
+  `soal` varchar(255) NOT NULL,
+  `id_paket` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_hasil`
+--
+ALTER TABLE `tb_hasil`
+  ADD PRIMARY KEY (`id_hasil`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_paket` (`id_paket`);
+
+--
+-- Indexes for table `tb_paket`
+--
+ALTER TABLE `tb_paket`
+  ADD PRIMARY KEY (`id_paket`);
+
+--
+-- Indexes for table `tb_soal`
+--
+ALTER TABLE `tb_soal`
+  ADD PRIMARY KEY (`id_soal`),
+  ADD KEY `id_paket` (`id_paket`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_hasil`
+--
+ALTER TABLE `tb_hasil`
+  ADD CONSTRAINT `tb_hasil_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`),
+  ADD CONSTRAINT `tb_hasil_ibfk_2` FOREIGN KEY (`id_paket`) REFERENCES `tb_paket` (`id_paket`);
+
+--
+-- Constraints for table `tb_soal`
+--
+ALTER TABLE `tb_soal`
+  ADD CONSTRAINT `tb_soal_ibfk_1` FOREIGN KEY (`id_paket`) REFERENCES `tb_paket` (`id_paket`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
