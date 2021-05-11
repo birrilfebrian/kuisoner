d5e2641a28312e12fe24af8fdb954f7f5a38bab0
birrilfebrian <55568576+birrilfebrian@users.noreply.github.com>
2021-05-11 23:08:14 +0700

    asdas

--- Web/application/controllers/api/Paket.php
+++ Web/application/controllers/api/Paket.php
@@ -13,28 +13,31 @@ class Paket extends REST_Controller {
   }
 
   public function index_post(){
-      $jenis = $this->get('jenis');
-      $id_anak = $this->get('id_anak');
+      $jenis = $this->post('jenis');
+      $id_anak = $this->post('id_anak');
       $kodehasil = $this->Kode->buatkode('id_hasil', 'tb_hasil', 'HS', '3');
 
-      $query = $this->db->query('SELECT count(tb_soal.id_soal) as jumlah_soal, * FROM tb_paket 
+      $query = $this->db->query('SELECT * FROM tb_paket 
       join tb_soal ON tb_paket.id_paket = tb_soal.id_paket 
-      Where tb_paket.jenis="'.$jenis.'" and tb_paket.umur=(select umur from tb_anak where id_anak = "'.$id_anak.'" ) ');
+      Where tb_paket.jenis="'.$jenis.'" and tb_paket.umur=(select umur from tb_anak where id_anak = "'.$id_anak.'" ) ')->result_array();
+      $cek = $this->db->query('SELECT count(tb_soal.id_soal) as jumlah_soal, tb_paket.id_paket, tb_paket.jenis, tb_soal.id_soal, tb_soal.soal FROM tb_paket 
+      join tb_soal ON tb_paket.id_paket = tb_soal.id_paket 
+      Where tb_paket.jenis="'.$jenis.'" and tb_paket.umur=(select umur from tb_anak where id_anak = "'.$id_anak.'" ) ')->row_array();
       
-      
-      if($query->num_rows() > 0){
+      if($cek['jumlah_soal'] > 0){
         $datah = array(
               'id_hasil' => $kodehasil,
-              'id_anak'  => $this->post('id_anak'),
+              'id_anak'  => $id_anak,
               'id_user'  => $this->post('id_user'),
-              'toal_point' => 0,
+              'total_point' => 0,
               'date'    => date('Y-m-d')
             );
             $cc = $this->db->insert('tb_hasil', $datah);
         if($cc){
+          $query['jumlah_soal'] = $cek['jumlah_soal'];
           $response = [
             'status' => true,
-            'data'   => $query->result_array()
+            'data'   => $query,
           ];
         }else{
           $response = [
@@ -45,7 +48,7 @@ class Paket extends REST_Controller {
       }else{
         $response = [
           'status' => false,
-          'message' => $query->num_rows()
+          'message' => "gagal"
         ];
       }
   
@@ -60,8 +63,8 @@ class Paket extends REST_Controller {
     $id_soal = $this->post('id_soal');
 
     $datad = array(
-      'id_detail_hasil' => $id_hasil,
-      'id_hasil'        => $kodehasil,
+      'id_detail_hasil' => $kodedhasil,
+      'id_hasil'        => $id_hasil,
       'id_soal'         => $id_soal,
       'jawaban'         => $jawab
     );
@@ -77,5 +80,33 @@ class Paket extends REST_Controller {
         'message' => 'Gagal upload tb_detail'
       ];
     }
+    $this->response($response, 200);
+  }
+  public function hasil_post(){
+    $id_hasil = $this->post('id_hasil');
+    $jum = $this->db->query('SELECT count(id_hasil) as jumlah_soal FROM tb_detail_hasil 
+      Where id_hasil = "'.$id_hasil.'"')->row_array();
+    $jwb = $this->db->query('SELECT jawaban FROM tb_detail_hasil 
+    Where id_hasil = "'.$id_hasil.'" and jawaban = "1"')->num_rows();
+
+    $hasil =  $jwb / $jum['jumlah_soal'];
+    $datad = array(
+      'total_point'        => $hasil
+    );
+    $this->db->set('total_point', $hasil, FALSE);
+    $this->db->where('id_hasil', $id_hasil);
+    $cc = $this->db->update('tb_hasil');
+    if($cc){
+      $response = [
+        'status' => true,
+        'message' => 'brhsal upload tb_detail'
+      ];
+    }else{
+      $response = [
+        'status' => false,
+        'message' => 'Gagal upload tb_detail'
+      ];
+    }
+    $this->response($response, 200);
   }
 }
