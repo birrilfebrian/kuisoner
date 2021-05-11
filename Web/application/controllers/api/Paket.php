98f41b483270649d42901c838a1790235430d2c5
birrilfebrian <55568576+birrilfebrian@users.noreply.github.com>
2021-05-11 22:15:23 +0700

    sasd

--- Web/application/controllers/api/Paket.php
+++ Web/application/controllers/api/Paket.php
@@ -12,19 +12,36 @@ class Paket extends REST_Controller {
         $this->load->model('Kode');
   }
 
-  public function index_get(){
+  public function index_post(){
       $jenis = $this->get('jenis');
       $id_anak = $this->get('id_anak');
+      $kodehasil = $this->Kode->buatkode('id_hasil', 'tb_hasil', 'HS', '3');
 
       $query = $this->db->query('SELECT count(tb_soal.id_soal) as jumlah_soal, * FROM tb_paket 
       join tb_soal ON tb_paket.id_paket = tb_soal.id_paket 
       Where tb_paket.jenis="'.$jenis.'" and tb_paket.umur=(select umur from tb_anak where id_anak = "'.$id_anak.'" ) ');
       
+      
       if($query->num_rows() > 0){
-        $response = [
-          'status' => true,
-          'data'   => $query->result_array()
-        ];
+        $datah = array(
+              'id_hasil' => $kodehasil,
+              'id_anak'  => $this->post('id_anak'),
+              'id_user'  => $this->post('id_user'),
+              'toal_point' => 0,
+              'date'    => date('Y-m-d')
+            );
+            $cc = $this->db->insert('tb_hasil', $datah);
+        if($cc){
+          $response = [
+            'status' => true,
+            'data'   => $query->result_array()
+          ];
+        }else{
+          $response = [
+            'status' => false,
+            'data'   => 'asdas'
+          ];
+        }
       }else{
         $response = [
           'status' => false,
@@ -38,42 +55,22 @@ class Paket extends REST_Controller {
 
   public function dhasil_post(){
     $kodedhasil = $this->Kode->buatkode('id_detail_hasil', 'tb_detail_hasil', 'DHS', '3');
-    $kodehasil = $this->Kode->buatkode('id_hasil', 'tb_hasil', 'HS', '3');
-  
+    $id_hasil = $this->post('id_hasil');
+    $jawab = $this->post('jawaban');
+    $id_soal = $this->post('id_soal');
+
     $datad = array(
-      'id_detail_hasil' => $kodedhasil,
+      'id_detail_hasil' => $id_hasil,
       'id_hasil'        => $kodehasil,
-      'id_soal'         => $this->post('id_soal'),
-      'jawaban'         => $this->post('jawaban')
+      'id_soal'         => $id_soal,
+      'jawaban'         => $jawab
     );
-
-    $ss = $this->db->insert('tb_detail_hasil', $datad);
-
-    if($ss){
-
-      $caritotal = $this->db->query('SELECT sum(jawaban) as totals from tb_detail_hasil where id_hasil = "'.$kodehasil.'" ')->row_array();
-
-      $datah = array(
-        'id_hasil' => $kodehasil,
-        'id_anak'  => $this->post('id_anak'),
-        'id_user'  => $this->post('id_user'),
-        'toal_point' => $caritotal['total'],
-        'date'    => date('Y-m-d')
-      );
-
-      $cc = $this->db->insert('tb_hasil', $datah);
-
-      if($cc){
-        $response = [
-          'status' => true,
-          'message' => 'Berhasil'
-        ];
-      }else{
-        $response = [
-          'status' => false,
-          'message' => 'Gagal upload tb_hasil'
-        ];
-      }
+    $cc = $this->db->insert('tb_detail_hasil', $datad);
+    if($cc){
+      $response = [
+        'status' => true,
+        'message' => 'brhsal upload tb_detail'
+      ];
     }else{
       $response = [
         'status' => false,
